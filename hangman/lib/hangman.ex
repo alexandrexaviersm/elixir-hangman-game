a7b17cfb208a544081dc2bdd96dc730c62c71fd5
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2020-02-22 10:05:49 -0300

    Create a supervisor for the Hangman game

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,5 +1,8 @@
 defmodule Hangman do
-  def new_game, do: Hangman.Server.start_link()
+  def new_game do
+    {:ok, pid} = Supervisor.start_child(Hangman.Supervisor, [])
+    pid
+  end
 
   def tally(game_pid) do
     GenServer.call(game_pid, {:tally})
