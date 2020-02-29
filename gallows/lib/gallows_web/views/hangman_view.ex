19998531288f50b3f6ac84193b4c14bc88c4eb85
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2020-02-29 02:47:39 -0300

    Wrapping up: Add Graphics

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -2,6 +2,14 @@ defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
   import GallowsWeb.Views.Helpers.GameStateHelper
 
+  def turn(left, target) when target >= left do
+    "dim"
+  end
+
+  def turn(left, target) do
+    "faint"
+  end
+
   def new_game_button(conn) do
     button("New Game", to: GallowsWeb.Router.Helpers.hangman_path(conn, :create_game))
   end
