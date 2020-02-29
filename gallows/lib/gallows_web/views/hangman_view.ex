53896ec4c74b1c116e37edf4e0c2b0d6eb4f46f5
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2020-02-29 00:38:28 -0300

    Implement make_move

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,5 +1,14 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+  import GallowsWeb.Views.Helpers.GameStateHelper
+
+  def new_game_button(conn) do
+    button("New Game", to: GallowsWeb.Router.Helpers.hangman_path(conn, :create_game))
+  end
+
+  def game_over?(%{game_state: game_state}) do
+    game_state in [:won, :lost]
+  end
 
   def word_so_far(tally) do
     tally.letters |> Enum.join(" ")
